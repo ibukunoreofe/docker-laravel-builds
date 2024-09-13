54ff38067838b113414adce85f726d6d7f18070d
Ibukun Bello <ibukunoreofe@gmail.com>
2024-09-13 04:44:50 +0200

    allow other supervisor config on start on php 8.1

--- php8-1-ubuntu-22-04/start_laravel_app.sh
+++ php8-1-ubuntu-22-04/start_laravel_app.sh
@@ -3,7 +3,7 @@
 # Check if APP_ENV is set to "production"
 if [ "$APP_ENV" = "production" ]; then
     # If production, run supervisord as the main process
-    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+    /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 else
     # If not production, run the queue worker in the background
     php /var/www/html/artisan queue:work --sleep=3 --tries=3 --queue=default,emails &
